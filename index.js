7c48a61a4a4bf7f51364e7a79312395c95bd2415
João Ferreira <jpfn2407@gmail.com>
2021-11-04 08:46:19 +0000

    continente, auchan e intermache

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const app = express()
 
 var lista = []
 
+async function pegarPrecos(){
 
-function pegarPrecos(){
+    lista = []
+
     //Continente 
     
-    axios.get('https://www.continente.pt/bebidas-e-garrafeira/cervejas-e-sidras/cerveja-branca/?start=0&srule=categories%20top-sellers&pmin=0.01&sz=100')
+    await axios.get('https://www.continente.pt/bebidas-e-garrafeira/cervejas-e-sidras/cerveja-branca/?start=0&srule=categories%20top-sellers&pmin=0.01&sz=10000')
     .then((response)=>{
         const html = response.data
         const $ = cheerio.load(html)
@@ -35,26 +37,31 @@ function pegarPrecos(){
                     tamanho = $(childElem).find('div > div:nth-child(2)> div:nth-child(1) > p:nth-child(3)').text()
                 }
                 //.pid-2050624 > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > span:nth-child(1) > span:nth-child(1)
-                const preçoAtual = $(childElem).find('div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > span:nth-child(1) > span:nth-child(1)').text().trim().replace('€','')
+                const preçoAtual = $(childElem).find('div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > span:nth-child(1) > span:nth-child(1)').text().trim().replace('€','').replace(',','.')
         
                 //.pid-2050624 > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2) > span:nth-child(1)
                 var preçoAntigo = ''  
                 if($(childElem).find('div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2) > span:nth-child(1)').attr('content') != undefined){
-                    preçoAntigo = $(childElem).find(' div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2) > span:nth-child(1)').attr('content')
+                    preçoAntigo = $(childElem).find(' div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(2) > span:nth-child(1)').attr('content').replace(',','.')
                 } else {
                     preçoAntigo = preçoAtual
                 }
 
                 //.pid-2050624 > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > p:nth-child(3)
-                var desconto = ''
-                if($(childElem).find('div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > p:nth-child(3)').text() != ""){
-                    desconto = $(childElem).find('div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > p:nth-child(3)').text().split("Desconto Imediato: ").pop()
-                } else {
-                    desconto = "0%"
-                }
+                //var desconto = ''
+                //if($(childElem).find('div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > p:nth-child(3)').text() != ""){
+                //    desconto = $(childElem).find('div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > p:nth-child(3)').text().split("Desconto Imediato: ").pop()
+                //} else {
+                //    desconto = "0%"
+                //}
+              
+                //var desconto = ((parseFloat(preçoAntigo.replace(' €', '')) - parseFloat(preçoAtual.replace(' €', '')) / parseFloat(preçoAntigo.replace(' €', '')) )) * 100
+                var desconto = ((parseFloat(preçoAntigo) - parseFloat(preçoAtual))/parseFloat(preçoAntigo)) * 100
+                //var desconto = (parseFloat(preçoAtual) * 100 / parseFloat(preçoAntigo)) 
+                desconto = desconto.toFixed(0) + '%'
 
                 //.pid-2050624 > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)
-                const preçoPorLitro = $(childElem).find('div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)').text().trim().replace('€', '')
+                const preçoPorLitro = $(childElem).find('div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)').text().trim().replace('€', '').replace(',','.')
                 
                 if (marca != ''){
                     lista.push({
@@ -71,13 +78,11 @@ function pegarPrecos(){
             })
             
         })
- 
-
     })
     
 
     //Auchan
-    axios.get('https://www.auchan.pt/pt/bebidas-e-garrafeira/cervejas/cerveja-nacional/?prefn1=soldInStores&prefv1=000&sz=100')
+    await axios.get('https://www.auchan.pt/pt/bebidas-e-garrafeira/cervejas/cerveja-nacional/?prefn1=soldInStores&prefv1=000&sz=10000')
     .then((response)=>{
         const html = response.data
         const $ = cheerio.load(html)
@@ -115,11 +120,12 @@ function pegarPrecos(){
                     preçoAntigo = preçoAtual
                 }
 
+
                 var desconto = ((parseFloat(preçoAntigo.replace(' €','')) - parseFloat(preçoAtual.replace(' €',''))) / parseFloat(preçoAntigo.replace(' €','')) ) * 100
                 desconto = desconto.toFixed(0) + '%'
 
                 //div.auc-product:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > span:nth-child(1)
-                const preçoPorLitro = $(childElem).find('div:nth-child(3) > div:nth-child(1) > span:nth-child(1)').text().trim().split(' ')[0]
+                const preçoPorLitro = $(childElem).find('div:nth-child(3) > div:nth-child(1) > span:nth-child(1)').text().trim().split(' ')[0].replace(',','.')
                 
                 if (marca != ''){
                     lista.push({
@@ -138,26 +144,89 @@ function pegarPrecos(){
         })
  
     })
+
+
+    //Intermache
+    await axios.get('https://lojaonline.intermarche.pt/69-agualva---cacem/rayon/bebidas/cervejas/10340-cervejas-com-alcool')
+    .then((response)=>{
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        //li.vignette_produit_info:nth-child(1)
+        //li.vignette_produit_info:nth-child(71)
+        const elemSelector = 'li.vignette_produit_info'
+
+        $(elemSelector).each((index, elem) => {
+
+            const superMercado = 'Intermache'
+            //li.vignette_produit_info:nth-child(1) > div:nth-child(1) > div:nth-child(3) > p:nth-child(1)
+            //li.vignette_produit_info:nth-child(71) > div:nth-child(1) > div:nth-child(3) > p:nth-child(1)
+            const marca = $(elem).find('li.vignette_produit_info > div:nth-child(1) > div:nth-child(3) > p:nth-child(1)').text()
+
+            //li.vignette_produit_info:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(3) > span:nth-child(1)
+            const tamanho = $(elem).find('li.vignette_produit_info > div:nth-child(1) > div:nth-child(3) > div:nth-child(3) > span:nth-child(1)').text()
+
+            //li.vignette_produit_info:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > p:nth-child(2)
+            //li.vignette_produit_info:nth-child(3) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > p:nth-child(2)
+            const preçoAtual = $(elem).find('li.vignette_produit_info > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > p:nth-child(2)').text().replace(',','.').replace(' €', '')
+
+            //li.vignette_produit_info:nth-child(3) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > del:nth-child(1)
+            //li.vignette_produit_info:nth-child(3) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > del:nth-child(1)
+            var preçoAntigo = ''
+                if($(elem).find('li.vignette_produit_info > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > del:nth-child(1)').text() != ''){
+                    preçoAntigo = $(elem).find('li.vignette_produit_info > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > del:nth-child(1)').text().replace(',','.').replace(' €', '')
+                } else {
+                    preçoAntigo = preçoAtual
+                }
+
+            var desconto = ((parseFloat(preçoAntigo.replace(' €','')) - parseFloat(preçoAtual.replace(' €',''))) / parseFloat(preçoAntigo.replace(' €','')) ) * 100
+            desconto = desconto.toFixed(0) + '%'
+
+
+            //li.vignette_produit_info:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > p:nth-child(3)
+            const preçoPorLitro = $(elem).find('li.vignette_produit_info > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > p:nth-child(3)').text().trim().split(' ')[0].replace(',','.')
+
+            if (marca != ''){
+                lista.push({
+                    superMercado,
+                    marca,
+                    tamanho,
+                    preçoAtual,
+                    preçoAntigo,
+                    desconto,
+                    preçoPorLitro
+                    })        
+            }
+        })
+    })
+    return lista;
+
 }
+
 
 app.get('/', (req, res) => {
     res.json("Bem vindo ao website da cerveja")
 })
 
-app.get('/cerveja', (req, res) => {
+
+app.get('/cerveja', async (req, res) => {
     pegarPrecos()
-    res.json(lista)
+    .then(response => {
+        //console.log(lista);
+        res.json(lista); 
+    })
 })
 
 app.get('/cerveja/:marca', (req, res) => {
 
     pegarPrecos()
+    .then(response => {
+        var marcaId = req.params.marca
+        marcaId = marcaId.toUpperCase()
 
-    var marcaId = req.params.marca
-    marcaId = marcaId.toUpperCase()
-
-    res.json(lista.filter(item => item.marca.toUpperCase().includes(marcaId)))
-
+        res.json(lista.filter(item => item.marca.toUpperCase().includes(marcaId)))
+    })
+ 
 })
 
 app.listen(PORT, () => {
